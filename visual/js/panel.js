8a9221b0db0c4b8255743d914c32aa785d63d695
Neha Singhal <nehasinghal076@gmail.com>
2020-07-03 14:43:44 +0530

    Removed position of grid size thing

--- visual/js/panel.js
+++ visual/js/panel.js
@@ -19,9 +19,9 @@ var Panel = {
             top: $algo.offset().top + $algo.outerHeight() + 20
         });
         
-        $('#GridSize').css({
-            top: $('#instructions_panel').offset().top + $('#instructions_panel').outerHeight() + 20
-        });
+  //      $('#GridSize').css({
+  //          top: $('#instructions_panel').offset().top + $('#instructions_panel').outerHeight() + 20
+  //      });
       
         $('#button2').attr('disabled', 'disabled');
     },
@@ -190,11 +190,16 @@ var Panel = {
     getGridSize: function() {
          var ele = document.getElementsByName('grid'); 
               
-            for(i = 0; i < ele.length; i++) {           
-                if(ele[i].type="radio" && ele[i].checked ) {
+            for(i = 0; i < ele.length; i++) { 
+                  
+                if(ele[i].type="radio") { 
+                  
+                    if(ele[i].checked){
                         console.log(ele[i].value);
                         return ele[i].value ; 
+                     }
                 } 
-            }   
+            } 
+   
     }
 };
